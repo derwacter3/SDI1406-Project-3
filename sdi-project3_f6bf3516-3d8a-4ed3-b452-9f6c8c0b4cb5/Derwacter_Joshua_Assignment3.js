8c8fc7f7698238b79ad2a7ec144861338300e8fb
derwacter3 <derwacter3@fullsail.edu>
2014-06-19 07:35:14 -0400

    Update Derwacter_Joshua_Assignment3.js
    Commit Two

--- sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Derwacter_Joshua_Assignment3.js
+++ sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Derwacter_Joshua_Assignment3.js
@@ -13,3 +13,27 @@ var basketballGame = false,
 		ubwear: "", // upper body wear
 		lbwear: "", // lower body wear
 	},
+
+var JSON = {
+	"Positions": {
+				"Point Guard":	["Stamina", 80, "Injury", 20],
+				"Shooting Guard":	["Stamina", 75, "Injury", 24],
+				"Small Forward":	["Stamina", 72, "Injury", 26],
+				"Power Forward":	["Stamina", 68, "Injury", 36],
+				"Center":	["Stamina", 62, "Injury", 45],
+			},
+	"Headwear": {
+				"Headband":	["Inside Shooting", 5, "Passing", 5],
+				"Goggles":	["Outside Shooting", 5, "Dribbling", 5],
+			},
+	"Upper Body Wear": {
+						"Under Shirt":	["Stamina", 4, "Passing", 3],
+						"Wrist Band":	["Injury", 3, "Dribbling", 6],
+						"Arm Band":	["Outside Shooting", 5, "Stamina", 3],
+					},
+	"Lower Body Wear": {
+						"Knee Brace":	["Inside Shooting", 3, "Dribbling", 2],
+						"Shin Sleeve":	["Injury", 4, "Stamina", 1],
+						"Shoes":	["Passing", 7, "Dribbling", 7]
+					}
+};
