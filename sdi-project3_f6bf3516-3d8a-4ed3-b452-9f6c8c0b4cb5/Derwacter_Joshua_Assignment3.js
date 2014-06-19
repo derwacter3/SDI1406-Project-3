33d20e0ba6862db1894edc7d69e4221b4e1bae19
derwacter3 <derwacter3@fullsail.edu>
2014-06-19 05:59:17 -0400

    Update Derwacter_Joshua_Assignment3.js
    First Commit

--- sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Derwacter_Joshua_Assignment3.js
+++ sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5/Derwacter_Joshua_Assignment3.js
@@ -1,1 +1,15 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//Global Variables
+var basketballGame = false,
+	basketballPlayer = {
+		name: "",
+		position: "",
+		inside shooting: 0,
+		outside shooting: 0,
+		dribbling: 0,
+		passing: 0,
+		hwear: "",	// headwear
+		ubwear: "", // upper body wear
+		lbwear: "", // lower body wear
+	},
